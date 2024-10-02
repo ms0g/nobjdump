d400c943f14b3f04612c5c0f5599f1871ab5fcc8
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-10-02 14:55:19 +0300

    refactor(decoder): disassemble

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -8,9 +8,9 @@
 #define MAGIC2 0x53
 #define MAGIC3 0x1A
 
-#define INES_HEADER_SIZE 0x10
-#define PRG_ROM_SIZE 0x4000
-#define CHR_ROM_SIZE 0x2000
+#define INES_HEADER_SIZE    0x10
+#define PRG_ROM_SIZE        0x4000
+#define CHR_ROM_SIZE        0x2000
 
 InstructionDecoder::InstructionDecoder(const std::string& filename) {
     mRomFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
@@ -58,6 +58,7 @@ void InstructionDecoder::decode(Option opt) {
             displayHeader();
             break;
         case Option::DISASSEMBLE:
+            disassemble();
             break;
         case Option::PRG:
             displayPRG();
@@ -94,5 +95,88 @@ void InstructionDecoder::fmtDisplay(const std::vector<uint8_t>& data, uint16_t i
 }
 
 void InstructionDecoder::disassemble() {
-
+    for (const auto& op: mPrgData.data) {
+        switch (static_cast<Opcode>(op)) {
+            case Opcode::RTI:
+                std::cout << "RTI" << "\t";
+                break;
+            case Opcode::RTS:
+                std::cout << "RTS" << "\t";
+                break;
+            case Opcode::BRK:
+                std::cout << "BRK" << "\t";
+                break;
+            case Opcode::PHP:
+                std::cout << "PHP" << "\t";
+                break;
+            case Opcode::PLP:
+                std::cout << "PLP" << "\t";
+                break;
+            case Opcode::PHA:
+                std::cout << "PHA" << "\t";
+                break;
+            case Opcode::PLA:
+                std::cout << "PLA" << "\t";
+                break;
+            case Opcode::DEY:
+                std::cout << "DEY" << "\t";
+                break;
+            case Opcode::TAY:
+                std::cout << "TAY" << "\t";
+                break;
+            case Opcode::INY:
+                std::cout << "INY" << "\t";
+                break;
+            case Opcode::INX:
+                std::cout << "INX" << "\t";
+                break;
+            case Opcode::BCS:
+                std::cout << "BCS" << "\t";
+                break;
+            case Opcode::BCC:
+                std::cout << "BCC" << "\t";
+                break;
+            case Opcode::BEQ:
+                std::cout << "BEQ" << "\t";
+                break;
+            case Opcode::BNE:
+                std::cout << "BNE" << "\t";
+                break;
+            case Opcode::BPL:
+                std::cout << "BPL" << "\t";
+                break;
+            case Opcode::BMI:
+                std::cout << "BMI" << "\t";
+                break;
+            case Opcode::BVC:
+                std::cout << "BVC" << "\t";
+                break;
+            case Opcode::BVS:
+                std::cout << "BVS" << "\t";
+                break;
+            case Opcode::SEC:
+                std::cout << "SEC" << "\t";
+                break;
+            case Opcode::SEI:
+                std::cout << "SEI" << "\t";
+                break;
+            case Opcode::SED:
+                std::cout << "SED" << "\t";
+                break;
+            case Opcode::CLD:
+                std::cout << "CLD" << "\t";
+                break;
+            case Opcode::CLV:
+                std::cout << "CLV" << "\t";
+                break;
+            case Opcode::CLC:
+                std::cout << "CLC" << "\t";
+                break;
+            case Opcode::CLI:
+                std::cout << "CLI" << "\t";
+                break;
+            default:
+                std::cout << "Unknown OPCODE" << "\t";
+        }
+    }
 }
